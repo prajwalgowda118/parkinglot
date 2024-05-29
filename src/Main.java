8b6e0e0bbac2a054c1d372f9d28b2156d9a4a1de
prajwalgowda118 <prajwal7683@gmail.comgit config --global user.name prajwalgowda118git config --global user.email prajwal7683@gmail.com>
2024-05-29 17:59:08 +0100

    added the test case and parking lot

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,39 @@
 import parking.ParkingLevel;
+import parking.ParkingLot;
 import vehicle.VehicleClass;
 import vehicle.car;
+import vehicle.truck;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        VehicleClass car1= new car("prajwalcar1");
-        VehicleClass car2= new car("prajwalcar2");
-
-        VehicleClass Truck1= new car("Truck1");
-        VehicleClass Truck2= new car("Truck1");
-
-       /* ParkingSpot spot1=new ParkingSpot(1);
-
-        System.out.println(spot1.isAvailable());
-        spot1.park(car1);
-        System.out.println(spot1.isAvailable());
-        System.out.println(spot1);
-        spot1.unpark();
-        System.out.println(spot1.isAvailable());*/
+        VehicleClass car1 = new car("prajwalcar1");
+        VehicleClass car2 = new car("prajwalcar2");
+        VehicleClass truck1 = new truck("Truck1");
+        VehicleClass truck2 = new truck("Truck2");
 
         ParkingLevel level1 = new ParkingLevel(1, 10);
+        ParkingLevel level2 = new ParkingLevel(2, 10);
 
-        // Park the vehicles
-        System.out.println("Parking car1: " + level1.parkVehicel(car1));
-        System.out.println("Parking car2: " + level1.parkVehicel(car2));
+        ParkingLot lot1 = ParkingLot.getInstance();
+        lot1.addLev(level1);
+        lot1.addLev(level2);
 
-        // Display the status of the parking level
-        level1.display();
+        System.out.println("Parking car1: " + lot1.parkVehicle(car1));
+        System.out.println("Parking car2: " + lot1.parkVehicle(car2));
+        System.out.println("Parking truck1: " + lot1.parkVehicle(truck1));
+        System.out.println("Parking truck2: " + lot1.parkVehicle(truck2));
+
+        // Display the status of the parking lot
+        lot1.display();
 
         // Unpark one vehicle
-        System.out.println("Unparking car1: " + level1.unpark(car1));
+        System.out.println("Unparking car1: " + lot1.unPark(car1));
 
-        // Display the status of the parking level again
-        //level1.display();
+        // Display the status of the parking lot again
+        lot1.display();
 
     }
 }
--- src/parking/ParkingLot.java
+++ src/parking/ParkingLot.java
@@ -1,5 +1,59 @@
 package parking;
 
+import vehicle.VehicleClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
 
+    private static ParkingLot instance;
+    private final List<ParkingLevel> levels;
+
+    private ParkingLot() {
+        levels = new ArrayList<>();
+    }
+
+    public static ParkingLot getInstance() {
+        if (instance == null) {
+            instance = new ParkingLot();
+        }
+        return instance;
+    }
+
+    public void addLev(ParkingLevel level) {
+        levels.add(level);
+    }
+
+    public List<ParkingLevel> getLevels() {
+        return levels;
+    }
+
+    public boolean parkVehicle(VehicleClass Vehicle){
+
+        for(ParkingLevel spot: levels){
+            if(spot.parkVehicel(Vehicle)){
+                //spot.park(veh);
+                return true;
+            }
+        }
+        System.out.println("No available spots for vehicle type: " +Vehicle.getType());
+
+        return false;
+    }
+    public boolean unPark(VehicleClass Vehicle){
+
+        for(ParkingLevel spot: levels){
+            if(spot.parkVehicel(Vehicle)){
+                //spot.park(veh);
+                return true;
+            }
+        }
+        return false;
+    }
+    public void display(){
+        for (ParkingLevel level : levels) {
+            level.display();
+        }
+    }
 }
